e0c5d3ee3085801f507765e7126d3be3e1ee9132
Arinadi Nur Rohmad <arinadi.nur@gmail.com>
2019-06-13 09:13:36 +0700

    create simple routing

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers)
 
+    const url = req.url;
+
+    //simple routing
+    if(url === '/msg') {
+        res.setHeader("Content-Type", "text/html");
+        res.write(`<html>`);
+        res.write(`<title>MSG</title>`);
+        res.write(`<b>Halo, Method is ${req.method} </b>`);
+        res.write(`</html>`);
+        return res.end();
+    }
+
     res.setHeader("Content-Type", "text/html");
     res.write(`<html>`);
     res.write(`<title>TEST</title>`);
