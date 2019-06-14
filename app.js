6c15da2a37b43c62d873b40f5ee894dca8994da0
Arinadi Nur Rohmad <arinadi.nur@gmail.com>
2019-06-14 08:34:54 +0700

    get data post and write data

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
+const fs = require('fs');
+
 const http = require('http');
 
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers)
+    // console.log(req.url, req.method, req.headers)
 
     const url = req.url;
 
     //simple routing
-    if(url === '/msg') {
-        res.setHeader("Content-Type", "text/html");
-        res.write(`<html>`);
-        res.write(`<title>MSG</title>`);
-        res.write(`<b>Halo, Method is ${req.method} </b>`);
-        res.write(`</html>`);
+    if (url === '/msg') {
+        const body = [];
+
+        // get DATA POST
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+            // console.log(body);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const msg = parsedBody.split('=')[1];
+            console.log(msg);
+            //Write File
+            fs.writeFileSync('msg.txt', msg);
+        });
+
+        //Redirect
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+
+        // res.setHeader("Content-Type", "text/html");
+        // res.write(`<html>`);
+        // res.write(`<title>MSG</title>`);
+        // res.write(`<b>Halo, Method is ${req.method} </b>`);
+        // res.write(`</html>`);
         return res.end();
     }
 
