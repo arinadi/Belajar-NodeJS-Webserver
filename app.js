e97f4be50cbc0366fd96033ba24f1dc48aae12ee
Arinadi Nur Rohmad <arinadi.nur@gmail.com>
2019-06-14 12:50:38 +0700

    make external module

--- app.js
+++ app.js
@@ -1,49 +1,13 @@
 const fs = require('fs');
 
 const http = require('http');
+const routes = require('./routes');
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 const port = 3000;
-
-const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers)
-
-    const url = req.url;
-
-    //simple routing
-    if (url === '/msg') {
-        const body = [];
-
-        // get DATA POST
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-            // console.log(body);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const msg = parsedBody.split('=')[1];
-            console.log(msg);
-            //Write File
-            fs.writeFile('msg.txt', msg, err => {
-                //Redirect
-                res.statusCode = 302;
-                res.setHeader("Location", "/");
-                return res.end();
-            });
-        });
-    }
-
-    res.setHeader("Content-Type", "text/html");
-    res.write(`<html>`);
-    res.write(`<title>TEST</title>`);
-    res.write(`<b>Halo, Method is ${req.method} </b>`);
-    res.write(`</html>`);
-    res.end();
-
-    // one shoot process
-    // process.exit();
-});
 
 // RUN
 server.listen(port);
